c637bf7dd9d191b97f9e80a863abdb553c2c210d
Evan Suma Rosenberg <suma@umn.edu>
2022-01-18 20:16:49 -0600

    assignment description and code comments

--- README.md
+++ README.md
@@ -2,21 +2,30 @@
 
 **Due: Monday, January 31, 11:59pm CDT**
 
-Description TBA.
+This assignment involves creating a simple, fun game using 2D graphics. You will learn to:
+
+- Use TypeScript and Node.js for some serious programming
+- Work with 2D graphics coordinate systems and primitives
+- Animate computer graphics based on user input
+- Implement game loops and event handlers
+
+ In general, assignments in this class are intended to be implemented in a step-by-step manner.  The rubric below lists all of the features you should implement, starting with the basic functionality and then working up to more advanced features.
+
+You can try a [finished version of the game](https://csci-4611-spring-2022.github.io/Builds/Assignment-1/) in the Builds repository on the course GitHub.
 
 ## Prerequisites
 
-To work with this code, you will need to install [Node.js](https://nodejs.org/en/) and [Visual Studio Code](https://code.visualstudio.com/). 
+To work with this code, you will first need to install [Node.js](https://nodejs.org/en/) and [Visual Studio Code](https://code.visualstudio.com/). 
 
 ## Getting Started
 
-The code implements the general structure that we reviewed in lecture.  You set up the initial project by pulling the dependencies from npm with:
+The code implements the general structure that we reviewed in lecture.  You set up the initial project by pulling the dependencies from the node package manager with:
 
 ```
 npm install
 ```
 
-After that, you can compile and run a server with:
+This will create a `node_modules` folder in your directory and download all the dependencies needed to run the project.  After that, you can compile and run a server with:
 
 ```
 npm run start
@@ -30,9 +39,25 @@ Graded out of 100 points.
 
 TBA.
 
+## Wizards
+
+All of the assignments in the course will include great opportunities for students to go beyond the requirements of the assignment and do cool extra work. However, we do offer a chance to show off… while grading the assignments the TAs will identify the best 4 or 5 examples of people doing cool stuff with computer graphics. We call these students our "wizards," and after each assignment, the students selected as wizards will get a chance to demonstrate their programs to the class!
+
+For this assignment, 
+
 ## Submission
 
-Instructions TBA.
+When you commit and push your assignment to GitHub, an automated script will build and deploy the production code to the `gh-pages` branch of your repository.  However, your submission is not complete until you do the following:
+
+1. Open your repository on GitHub and go to Settings->Pages.
+2. Change the source to the `gh-pages` branch, then save.
+3. Wait a couple minutes for the website to deploy.
+4. Test your build by pointing your web browser to:
+   https://csci-4611-spring-2022.github.io/your-repo-name-here/
+
+![GitHub screenshot](./images/github.png)
+
+If your program runs correctly, then you are finished!  This will indicate to the TAs that your assignment is ready for grading.  If you change your mind and want to make further changes to your code, then just set the GitHub pages source back to `None` and it will unpublish the website.
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+/* Assignment 1: Space Minesweeper
+ * CSCI 4611, Spring 2022, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
 import * as paper from 'paper';
 
 class Game 
 {
+    // Width and height are defined in project coordinates
+    // This is different than screen coordinates!
     private width : number;
     private height : number;
+
+    // TypeScript will throw an error if you define a type but don't initialize in the constructor
+    // This can be prevented by including undefined as a second possible type
     private ship : paper.Group | undefined;
     
     constructor()
@@ -18,6 +29,7 @@ class Game
         this.createScene();
         this.resize();
 
+        // This registers the event handlers for window and mouse events
         paper.view.onResize = () => {this.resize();};
         paper.view.onMouseMove = (event: paper.MouseEvent) => {this.onMouseMove(event);};
         paper.view.onMouseDown = (event: paper.MouseEvent) => {this.onMouseDown(event);};
@@ -27,21 +39,35 @@ class Game
 
     private createScene() : void 
     {
+        // Create a new group to hold the ship graphic
         this.ship = new paper.Group();
-        this.ship!.applyMatrix = false;
+
+        // This line prevents the transformation matrix from being baked directly into its children
+        // Instead, will be applied every frame
+        this.ship.applyMatrix = false;
+
+        // This code block loads an SVG file asynchronously
+        // It uses an arrow function to specify the code that gets executed after the file is loaded
+        // We will go over this syntax in class
         paper.project.importSVG('./assets/ship.svg', (item: paper.Item) => {
+            // The exclamation point tells TypeScript you are certain the variable has been defined
             item.addTo(this.ship!);
             this.ship!.scale(3);
             this.ship!.position.x = this.width / 2;
             this.ship!.position.y = this.height / 2;
         });
+
+        // Add more code here
     }
 
+    // This method will be called once per frame
     private update(event: GameEvent) : void
     {
-        // This method will be called once per frame
+        // Add code here
     }
 
+    // This handles dynamic resizing of the browser window
+    // You do not need to modify this function
     private resize() : void
     {
         var aspectRatio = this.width / this.height;
@@ -57,9 +83,11 @@ class Game
 
     private onMouseMove(event: paper.MouseEvent) : void
     {
+        // Get the vector from the center of the screen to the mouse position
         var mouseVector = event.point.subtract(paper.view.center);
+
+        // Point the ship towards the mouse cursor by converting the vector to an angle
         this.ship!.rotation = mouseVector.angle + 90;
-        this.ship!.translate(new paper.Point(1,0));
     }
 
     private onMouseDown(event: paper.MouseEvent) : void
@@ -68,6 +96,8 @@ class Game
     } 
 }
 
+// This is included because the paper is missing a TypeScript definition
+// You do not need to modify it
 class GameEvent
 {
     readonly delta: number;
@@ -82,6 +112,6 @@ class GameEvent
     }
 }
     
-// start the game
+// Start the game
 var game = new Game();
 game.start();
